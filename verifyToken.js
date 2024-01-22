c6cb1bcb1f3faa6144657d0a00a7f173a3f67533
Ali Haider <ah712200@Gmail.com>
2024-01-22 19:45:14 +0500

    code update

--- verifyToken.js
+++ verifyToken.js
@@ -1,28 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ error: "You are not authenticated!" });
-  }
-
-  jwt.verify(token, process.env.SECRET, (err, data) => {
-    if (err) {
-      if (err.name === "TokenExpiredError") {
-        return res.status(401).json({ error: "Token has expired" });
-      }
-      return res.status(403).json({ error: "Token is not valid" });
+const verifyToken=(req,res,next)=>{
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json("You are not Authenticated!")
     }
-
-    // Attach user information to the request
-    req.userId = data.id;
-
-    // Optionally, you can include user information in the response
-    req.user = { id: data.id, username: data.username };
-
-    next();
-  });
-};
-
-module.exports = verifyToken;
+    jwt.verify(token,process.env.SECRET,async(err,data)=>{
+        if(err){
+            return res.status(403).json("Token Is Not Valid!")
+        }
+        req.userId=data.id
+        next()
+    })
+}
+module.exports = verifyToken
